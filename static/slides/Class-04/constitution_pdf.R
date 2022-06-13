1c8aec4c052759023150d8d28c181740a90767bb
Josh Allen <jallen108@gsu.edu>
2022-06-13 10:53:20 -0400

    addd pdf version of the slides

--- static/slides/Class-04/constitution_pdf.R
+++ static/slides/Class-04/constitution_pdf.R
@@ -1,1 +1,7 @@
-xaringan::decktape("Class-04/constitution.html", output = "Class-04/constitution.pdf")
+xaringan::decktape("https://spols110122.netlify.app/slides/class-05/federalism", output = "Class-05/federalism.pdf")
+
+
+source("https://git.io/xaringan2pdf")
+
+
+xaringan_to_pdf("https://spols110122.netlify.app/slides/class-05/federalism", output_file = "Class-05/federalism.pdf", include_partial_slides = TRUE)
